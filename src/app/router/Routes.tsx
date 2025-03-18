d1a8f95d5e11d884c9e9a91d4cd576c58da0fb6a
William Cao <williamcao.cmu@gmail.com>
2025-03-18 20:35:23 +0700

    02: activity details

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router";
 
 import ActivityDashboard from "@/features/activities/dashboard/ActivityDashboard";
-import ActivityDetail from "@/features/activities/details/ActivityDetail";
+import ActivityDetailPage from "@/features/activities/details/ActivityDetailPage";
 import ActivityForm from "@/features/activities/form/ActivityForm";
 import HomePage from "@/features/home/HomePage";
 import App from "@/app/layout/App";
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "activities", element: <ActivityDashboard /> },
-      { path: "activities/:id", element: <ActivityDetail /> },
+      { path: "activities/:id", element: <ActivityDetailPage /> },
       { path: "createActivity", element: <ActivityForm key="create" /> },
       { path: "manage/:id", element: <ActivityForm /> },
     ],
