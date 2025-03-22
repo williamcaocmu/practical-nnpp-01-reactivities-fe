f8b383530011f9c1864b9894fc6d05a1a8a8ac01
William Cao <williamcao.cmu@gmail.com>
2025-03-23 01:30:17 +0700

    06: react dropzone

--- src/shared/components/PhotoUploadWidget.tsx
+++ src/shared/components/PhotoUploadWidget.tsx
@@ -1,15 +1,44 @@
-import { Grid2, Typography } from "@mui/material";
-import "cropperjs/dist/cropper.css";
+import { CloudUpload } from "@mui/icons-material";
+import { Box, Grid2, Typography } from "@mui/material";
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+// import "cropperjs/dist/cropper.css";
 
 type Props = {};
 
 export default function PhotoUploadWidget(props: Props) {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <Grid2 container spacing={3}>
       <Grid2 size={4}>
         <Typography variant="overline" color="secondary">
           Step 1 - Add photo
         </Typography>
+
+        <Box
+          {...getRootProps()}
+          sx={{
+            border: "dashed 3px #eee",
+            borderColor: isDragActive ? "green" : "#eee",
+            borderRadius: "5px",
+            paddingTop: "30px",
+            textAlign: "center",
+            height: "280px",
+          }}
+        >
+          <input {...getInputProps()} />
+          <CloudUpload
+            sx={{
+              fontSize: 80,
+            }}
+          />
+          <Typography variant="h5">Drop image here</Typography>
+        </Box>
       </Grid2>
       <Grid2 size={4}>
         <Typography variant="overline" color="secondary">
