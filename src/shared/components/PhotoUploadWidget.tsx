e806845d7df710ec44ce6eb06c58e6827db98625
William Cao <williamcao.cmu@gmail.com>
2025-03-23 11:27:16 +0700

    07: react cropper

--- src/shared/components/PhotoUploadWidget.tsx
+++ src/shared/components/PhotoUploadWidget.tsx
@@ -1,14 +1,29 @@
 import { CloudUpload } from "@mui/icons-material";
-import { Box, Grid2, Typography } from "@mui/material";
-import { useCallback } from "react";
+import { Box, Button as MuiButton, Grid2, Typography } from "@mui/material";
+import { useCallback, useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
-// import "cropperjs/dist/cropper.css";
+import { Cropper, ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 type Props = {};
 
 export default function PhotoUploadWidget(props: Props) {
+  const [files, setFiles] = useState<object & { preview: string }[]>([]);
+  const cropperRef = useRef<ReactCropperElement>(null);
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+  }, []);
+
+  const onCrop = useCallback(() => {
+    const cropper = cropperRef.current?.cropper;
+    cropper?.getCroppedCanvas().toBlob((blob) => {});
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
@@ -44,11 +59,41 @@ export default function PhotoUploadWidget(props: Props) {
         <Typography variant="overline" color="secondary">
           Step 2 - Resize image
         </Typography>
+
+        {files[0]?.preview && (
+          <Cropper
+            src={files[0]?.preview}
+            style={{ height: 300, width: "90%" }}
+            initialAspectRatio={1}
+            aspectRatio={1}
+            preview=".img-preview"
+            guides={false}
+            viewMode={1}
+            background={false}
+            ref={cropperRef}
+          />
+        )}
       </Grid2>
       <Grid2 size={4}>
-        <Typography variant="overline" color="secondary">
-          Step 3 - Preview & upload
-        </Typography>
+        {files[0]?.preview && (
+          <>
+            <Typography variant="overline" color="secondary">
+              Step 3 - Preview & upload
+            </Typography>
+            <div
+              className="img-preview"
+              style={{ width: 300, height: 300, overflow: "hidden" }}
+            />
+            <MuiButton
+              sx={{ my: 1, width: 300 }}
+              onClick={onCrop}
+              variant="contained"
+              color="secondary"
+            >
+              Upload
+            </MuiButton>
+          </>
+        )}
       </Grid2>
     </Grid2>
   );
